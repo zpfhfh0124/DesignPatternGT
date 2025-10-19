992356054e936a387b41a8670e611697cbc31270
zpfhfh0124 <48612497+zpfhfh0124@users.noreply.github.com>
2025-10-19 16:36:13 +0900

    커맨드 패턴 예제 업로드

---
+++ DesignPatternGT/Assets/Scripts/CommandPattern/BikeController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Chapter.Command
+{
+    public class BikeController : MonoBehaviour
+    {
+        public enum Direction
+        {
+            Left = -1,
+            Right = 1
+        }
+
+        private bool _isTurboOn = false;
+        private float _distance = 1.0f;
+        
+        public void ToggleTurbo()
+        {
+            _isTurboOn = !_isTurboOn;
+            Debug.Log($"Turbo Active: {_isTurboOn}");
+        }
+
+        public void Turn(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Left:
+                    transform.Translate(Vector3.left * _distance);
+                    break;
+                case Direction.Right:
+                    transform.Translate(Vector3.right * _distance);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void ResetPosition()
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ DesignPatternGT/Assets/Scripts/CommandPattern/Invoker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics.Geometry;
+using UnityEngine;
+
+namespace Chapter.Command
+{
+    public class Invoker : MonoBehaviour
+    {
+        bool _isRecording = false;
+        bool _isReplaying = false;
+        float _replayTime = 0f;
+        float _recordingTime = 0f;
+        SortedList<float, Command> _recordedCommands = new SortedList<float, Command>();
+
+        public void ExecuteCommand(Command command)
+        {
+            command.Execute();
+            
+            if(_isRecording) _recordedCommands.Add(_recordingTime, command);
+            
+            Debug.Log($"Recorded Time : {_recordingTime}");
+            Debug.Log($"Recorded Command : {command}");
+        }
+
+        public void Record()
+        {
+            _recordingTime = 0f;
+            _isRecording = true;
+        }
+
+        public void Replay()
+        {
+            _replayTime = 0f;
+            _isReplaying = true;
+
+            if (_recordedCommands.Count <= 0)
+            {
+                Debug.Log("No commands to replay!");
+                return;
+            }
+            
+            _recordedCommands.Reverse();
+        }
+
+        void FixedUpdate()
+        {
+            if(_isRecording) _recordingTime += Time.fixedDeltaTime;
+
+            if (_isReplaying)
+            {
+                _replayTime += Time.fixedDeltaTime;
+
+                if (_recordedCommands.Any())
+                {
+                    if (Mathf.Approximately(_replayTime, _recordedCommands.Keys[0]))
+                    {
+                        Debug.Log($"Replay Time : {_replayTime}");
+                        Debug.Log($"Replay Command : {_recordedCommands.First()}");
+                        
+                        _recordedCommands.Values[0].Execute();
+                        _recordedCommands.RemoveAt(0);
+                    }
+                }
+                else _isReplaying = false;
+            }
+        }
+    }
+}
